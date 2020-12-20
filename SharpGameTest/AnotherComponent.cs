3a98fd6f614fb7adcad47134e483c3432335aedc
Wither <wither.tweets@gmail.com>
2020-12-20 19:00:05 +0100

    Added another example component to test axis input

---
+++ SharpGameTest/AnotherComponent.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK;
+using OpenTK.Input;
+
+using SharpGame;
+using SharpGame.Objects.Components;
+using SharpGame.Util;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGameTest
+{
+    class AnotherComponent : Component
+    {
+        public override void OnUpdate(float deltaTime)
+        {
+            /*
+            MouseState newMouseState = Mouse.GetState();
+            if (Input.mouseState.HasValue)
+            {
+                //return (mouseAxis == MouseAxis.X) ? (newMouseState.Y - mouseState.Value.Y) : (mouseState.Value.X - newMouseState.X);
+                Logger.Info(Input.mouseState.Value.X - newMouseState.X);
+            }
+            Input.mouseState = newMouseState;
+            */
+            //this.Actor.RotationComponent.Rotate(-Input.GetAxis(MouseAxis.Y) * 0.05f, -Input.GetAxis(MouseAxis.X) * 0.05f, 0);
+           // Logger.Info((-Input.GetAxis(MouseAxis.X) * 0.05f) + " " + (-Input.GetAxis(MouseAxis.X) * 0.05f));
+        }
+    }
+}
