f6b6b7d5453f88325a057d5267172bbc25c98617
Wither <wither.tweets@gmail.com>
2020-12-21 15:33:53 +0100

    Started working on sound (kind of)

---
+++ SharpGame/Audio/Sound.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Audio.OpenAL;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGame.Audio
+{
+    public class Sound
+    {
+        private readonly int id;
+        private readonly int buffer;
+
+        public Sound()
+        {
+            id = AL.GenBuffer();
+        }
+    }
+}
