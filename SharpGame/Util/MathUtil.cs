c6ef014fff17dda0a9cfb7487d71a05a0224449d
Wither <wither.tweets@gmail.com>
2020-12-24 18:47:54 +0100

    Changed method names in MathUtil to better reflect their function

--- SharpGame/Util/MathUtil.cs
+++ SharpGame/Util/MathUtil.cs
@@ -11,14 +11,14 @@ namespace SharpGame.Util
 {
     internal static class MathUtil
     {
-        public static bool Vector3Similar(Vector3 a, Vector3 b)
+        public static bool Vector3ApproximatelyEqual(Vector3 a, Vector3 b)
         {
             return MathHelper.ApproximatelyEqualEpsilon(a.X, b.X, SharedConstants.VectorMargin) &&
                    MathHelper.ApproximatelyEqualEpsilon(a.Y, b.Y, SharedConstants.VectorMargin) &&
                    MathHelper.ApproximatelyEqualEpsilon(a.Z, b.Z, SharedConstants.VectorMargin);
         }
 
-        public static bool Vector2Similar(Vector2 a, Vector2 b)
+        public static bool Vector2ApproximatelyEqual(Vector2 a, Vector2 b)
         {
             return MathHelper.ApproximatelyEqualEpsilon(a.X, b.X, SharedConstants.VectorMargin) &&
                    MathHelper.ApproximatelyEqualEpsilon(a.Y, b.Y, SharedConstants.VectorMargin);
