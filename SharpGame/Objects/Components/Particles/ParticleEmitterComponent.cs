1c3ebe14ebc85398194c0fe076184376a87f227b
Wither <wither.tweets@gmail.com>
2021-01-05 15:18:01 +0100

    Started working on particles. Still trying to figue out what's the best way to approach them

---
+++ SharpGame/Objects/Components/Particles/ParticleEmitterComponent.cs
@@ -0,0 +1,25 @@
+﻿using SharpGame.Graphics;
+using SharpGame.Graphics.Meshes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGame.Objects.Components
+{
+    class ParticleEmitterComponent : MeshRendererComponent
+    {
+        public int Count { get; set; }
+        public float Velocity { get; set; }
+        public float Life { get; set; }
+
+        public ParticleEmitterComponent(int count, float velocity, float life, Material material) : base(Mesh.GuiQuad, material)
+        {
+            this.Count = count;
+            this.Velocity = velocity;
+            this.Life = life;
+        }
+    }
+}
