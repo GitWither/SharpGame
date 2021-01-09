353455d88a9e5631affb2fe5a72b4dd9f1f40f6b
Wither <wither.tweets@gmail.com>
2021-01-09 11:21:32 +0100

    Removed useless comparison functions from positioncomponent

--- SharpGame/Objects/Components/Transform/PositionComponent.cs
+++ SharpGame/Objects/Components/Transform/PositionComponent.cs
@@ -1,14 +1,5 @@
 ﻿using OpenTK;
 
-using SharpGame.Util;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SharpGame.Objects.Components
 {
     public class PositionComponent : Component
@@ -24,11 +15,6 @@ namespace SharpGame.Objects.Components
             this.Z = z;
         }
 
-        public void Set(Vector3 newPosition)
-        {
-            Set(newPosition.X, newPosition.Y, newPosition.Z);
-        }
-
         public void Set(float x, float y, float z)
         {
             this.X = x;
@@ -36,32 +22,11 @@ namespace SharpGame.Objects.Components
             this.Z = z;
         }
 
-        public void Translate(Vector3 delta)
-        {
-            this.X += delta.X;
-            this.Y += delta.Y;
-            this.Z += delta.Z;
-        }
-
         public void Translate(float x, float y, float z)
         {
             this.X += x;
             this.Y += y;
             this.Z += z;
-        }
-
-        public static PositionComponent operator -(PositionComponent a, PositionComponent b)
-        {
-            return new PositionComponent(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-            if (!(obj is PositionComponent)) return false;
-
-            PositionComponent pos = (PositionComponent)obj;
-            return pos.X == this.X && pos.Y == this.Y && pos.Z == this.Z;
         }
 
         public static implicit operator Vector3(PositionComponent positionComponent)
