8835c22fd9ed46637b9721a3a128b42096e3aa3f
Wither <wither.tweets@gmail.com>
2021-01-11 09:19:06 +0100

    Started testing particles with various positions

--- SharpGame/Graphics/Vaos/ParticleVertexArrayObject.cs
+++ SharpGame/Graphics/Vaos/ParticleVertexArrayObject.cs
@@ -1,6 +1,7 @@
 ﻿using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 
+using SharpGame.Graphics.Meshes;
 using SharpGame.Util;
 
 using System;
@@ -17,8 +18,23 @@ namespace SharpGame.Graphics.Vaos
         {
             this.Bind();
 
-            this.BindVectorArrayToBuffer(this.bufferIds[0], 0, this.MeshRenderer.Mesh.Vertices, true);
-            this.BindVectorArrayToBuffer(this.bufferIds[1], 1, this.MeshRenderer.Mesh.FaceTexCoords, true);
+            this.BindVectorArrayToBuffer(this.bufferIds[0], 0, Mesh.GuiQuad.Vertices, false);
+            this.BindVectorArrayToBuffer(this.bufferIds[1], 1, Mesh.GuiQuad.FaceTexCoords, false);
+            this.BindVectorArrayToBuffer(this.bufferIds[2], 2, new Vector3[] {
+                new Vector3(2, 2, 5),
+                new Vector3(1, 2, 5),
+                new Vector3(5, 7, 2),
+                new Vector3(2, 6, 5),
+                new Vector3(2, 3, 6),
+                new Vector3(5, 6, 2),
+                new Vector3(6, 5, 6),
+                new Vector3(8, 2, 2),
+                new Vector3(3, 5, 8),
+                new Vector3(6, 4, 9),
+                new Vector3(7, 2, 6),
+                new Vector3(3, 1, 7)
+            }, true);
+            GL.VertexAttribDivisor(2, 1);
 
             this.BindIndices(this.MeshRenderer.Mesh.Indices);
 
