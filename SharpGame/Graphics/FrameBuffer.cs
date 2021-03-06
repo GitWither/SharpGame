c3cad88a03520bc2d07cdd63fcc8d3afd473f336
GitWither <wither.tweets@gmail.com>
2021-03-06 11:45:22 +0100

    Started working on framebuffer support

---
+++ SharpGame/Graphics/FrameBuffer.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGame.Graphics
+{
+    internal class FrameBuffer : IDisposable
+    {
+        private readonly int id;
+        public FrameBuffer(int width, int height)
+        {
+            id = GL.GenFramebuffer();
+
+            //GL.BindFramebuffer(FramebufferTarget.)
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteFramebuffer(id);
+        }
+    }
+}
