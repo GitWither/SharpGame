1fb1dad7586c5c204ce739b621b4442dc4244c7e
Daniel <wither.tweets@gmail.com>
2022-08-11 17:03:00 +0200

    Added helper ImGui classes that depend on scope

---
+++ SharpEditor/Util/ScopedMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+
+namespace SharpEditor.Util
+{
+    public struct ScopedMenu : IDisposable
+    {
+
+        public ScopedMenu(string name)
+        {
+            ImGui.Begin(name);
+        }
+
+        public ScopedMenu(string name, ImGuiWindowFlags windowFlags)
+        {
+            ImGui.Begin(name, windowFlags);
+        }
+
+        public void Dispose()
+        {
+            ImGui.End();
+        }
+    }
+}
---
+++ SharpEditor/Util/ScopedStyle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+using OpenTK.Mathematics;
+using Vector2 = System.Numerics.Vector2;
+
+namespace SharpEditor.Util
+{
+    public struct ScopedStyle : IDisposable
+    {
+        public ScopedStyle(ImGuiStyleVar styleVar, float value)
+        {
+            ImGui.PushStyleVar(styleVar, value);
+        }
+
+        public ScopedStyle(ImGuiStyleVar styleVar, Vector2 value)
+        {
+            ImGui.PushStyleVar(styleVar, value);
+        }
+
+        public void Dispose()
+        {
+            ImGui.PopStyleVar();
+        }
+    }
+}
