620e7c681218c1d9b78c24225dfed02214a2db77
Daniel <wither.tweets@gmail.com>
2022-08-15 20:45:20 +0200

    Fixed Texture cleanup

--- SharpGame/Graphics/Texture.cs
+++ SharpGame/Graphics/Texture.cs
@@ -14,31 +14,35 @@ using System.Threading.Tasks;
 
 namespace SharpGame.Graphics
 {
-    public class Texture : IDisposable
+    public class Texture
     {
         private readonly int id;
 
         private readonly int height;
         private readonly int width;
 
-        public Texture(string file) : this((Bitmap)Image.FromFile(SharedConstants.TextureFolder + file + SharedConstants.TextureExtension))
+        public Texture(string file) : this(
+            (Bitmap)Image.FromFile(SharedConstants.TextureFolder + file + SharedConstants.TextureExtension))
         {
             Bitmap bitmap;
             try
             {
-                bitmap = (Bitmap)Image.FromFile(SharedConstants.TextureFolder + file + SharedConstants.TextureExtension);
+                bitmap = (Bitmap)Image.FromFile(SharedConstants.TextureFolder + file +
+                                                SharedConstants.TextureExtension);
             }
             catch (IOException e)
             {
                 Logger.Exception(e);
                 return;
             }
+
             Logger.Info("Successfully loaded texture " + file);
         }
 
         public Texture(Bitmap bitmap)
         {
-            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
             id = GL.GenTexture();
             height = bitmapData.Height;
@@ -46,12 +50,15 @@ namespace SharpGame.Graphics
 
             Bind(TextureUnit.Texture0);
 
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, bitmapData.Scan0);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0,
+                OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, bitmapData.Scan0);
 
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (float)TextureWrapMode.Repeat);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (float)TextureWrapMode.Repeat);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.NearestMipmapLinear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                (float)TextureMinFilter.NearestMipmapLinear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                (float)TextureMagFilter.Nearest);
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
             Unbind();
@@ -67,14 +74,14 @@ namespace SharpGame.Graphics
             GL.BindTexture(TextureTarget.Texture2D, id);
         }
 
-        public void Dispose()
-        {
-            GL.DeleteTexture(id);
-        }
-
         public void Unbind()
         {
             GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
+
+        ~Texture()
+        {
+            GL.DeleteTexture(id);
         }
     }
 }
