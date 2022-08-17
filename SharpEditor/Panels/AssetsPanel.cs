2c2ee4a9dd6246ddbe1738217434d1c534d80d3b
Daniel <wither.tweets@gmail.com>
2022-08-17 22:35:44 +0200

    Assets panel will now render a full path with clickable intermediate folders

--- SharpEditor/Panels/AssetsPanel.cs
+++ SharpEditor/Panels/AssetsPanel.cs
@@ -7,6 +7,7 @@ using ImGuiNET;
 using System.Numerics;
 using SharpEditor.Util;
 using SharpGame.Graphics;
+using SharpGame.Util;
 
 namespace SharpEditor.Panels
 {
@@ -20,21 +21,59 @@ namespace SharpEditor.Panels
         private Texture m_FolderIcon = new Texture("EditorAssets/Folder");
         private Texture m_FileIcon = new Texture("EditorAssets/File");
 
+        private string[] m_ParentPaths = Array.Empty<string>();
+
+        public AssetsPanel()
+        {
+            this.NavigateToPath(m_CurrentDirectory);
+        }
+
         public void OnImGuiRender()
         {
+            ImGui.ShowDemoWindow();
             using (new ScopedMenu("Assets"))
             {
+                ImGui.Button("Import File");
+                ImGui.SameLine();
+                ImGui.Button("Open in File Explorer");
+
+                ImGui.Separator();
+
                 if (ImGui.Button("<"))
                 {
-                    m_CurrentDirectory = Directory.GetParent(m_CurrentDirectory).FullName;
+                    NavigateToPath(Directory.GetParent(m_CurrentDirectory).FullName);
                 }
 
                 ImGui.SameLine();
 
-                ImGui.Text(m_CurrentDirectory);
+
+                using (new ScopedStyle(ImGuiStyleVar.ItemSpacing, Vector2.Zero))
+                using (new ScopedStyle(ImGuiStyleVar.FramePadding, new Vector2(0.5f, 2)))
+                using (new ScopedColor(ImGuiCol.Button, 0x00000000))
+                {
+                    foreach (string path in m_ParentPaths)
+                    {
+                        string pathName = Path.GetFileName(path);
+                        if (pathName != null)
+                        {
+                            if (ImGui.Button(pathName))
+                            {
+                                NavigateToPath(path);
+                            }
+                            ImGui.SameLine();
+                            ImGui.Text("/");
+                            ImGui.SameLine();
+                        }
+                    }
+                }
+
+
+                ImGui.NewLine();
+                //ImGui.Text(m_CurrentDirectory);
 
                 //ImGui.DragFloat("Thumb Size", ref thumbnailSize);
                 //ImGui.DragFloat("Padding", ref padding);
+
                 
                 Vector2 maxRegion = ImGui.GetContentRegionAvail();
                 int maxColumns = (int)(maxRegion.X / (thumbnailSize + padding));
@@ -53,12 +92,28 @@ namespace SharpEditor.Panels
 
                     if (isDirectory && ImGui.IsItemHovered() && ImGui.IsMouseDoubleClicked(ImGuiMouseButton.Left))
                     {
-                        m_CurrentDirectory = path;
+                        NavigateToPath(path);
                     }
 
                     ImGui.TextWrapped(Path.GetFileName(path));
 
                     ImGui.NextColumn();
+                }
+            }
+        }
+
+        private void NavigateToPath(string newPath)
+        {
+            m_CurrentDirectory = newPath;
+
+            m_ParentPaths = new string[newPath.Split(Path.DirectorySeparatorChar).Length];
+            DirectoryInfo info = new DirectoryInfo(newPath);
+            for (int i = m_ParentPaths.Length - 1; i > -1; i--)
+            {
+                m_ParentPaths[i] = info.FullName;
+                if (info.Parent != null)
+                {
+                    info = info.Parent;
                 }
             }
         }
